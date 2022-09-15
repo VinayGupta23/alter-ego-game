31c2d8f78c4796568d5910809c2449ca70604206
Vinay Gupta <vinayg@usc.edu>
2022-09-15 11:20:20 -0700

    Remove redundant field in CloneLife

--- Assets/Scripts/CloneLife.cs
+++ Assets/Scripts/CloneLife.cs
@@ -2,19 +2,18 @@ using UnityEngine;
 
 public class CloneLife : MonoBehaviour
 {
-    [SerializeField] private Transform clone;
     private Vector3 originalPosition;
     
     void Start()
     {
-        originalPosition = clone.transform.position;
+        originalPosition = transform.position;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("trap"))
         {
             // take clone to original position
-            clone.transform.position = originalPosition;
+            transform.position = originalPosition;
         }
     }
  

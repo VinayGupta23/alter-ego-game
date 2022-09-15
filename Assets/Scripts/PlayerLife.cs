8e12d47907f8851e7a96ffe2513513c193a9ed74
venkata naresh chilakurthi <chilakur@usc.edu>
2022-09-15 09:13:48 +0530

    Added trap prefab

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("trap"))
+        {
+            rb.bodyType = RigidbodyType2D.Static;
+            Destroy(rb.gameObject);
+            Thread.Sleep(1000);
+            // Restarting the current level
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+ 
+}

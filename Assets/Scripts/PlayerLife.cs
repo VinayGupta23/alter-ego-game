9ccdb1f5924609b3481ad1f379705c9b5071b328
Vinay Gupta <vinayg@usc.edu>
2022-09-19 22:07:00 -0700

    Add death animation and rework life handling

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -1,24 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerLife : MonoBehaviour
+public class PlayerLife : LifeBase
 {
-    private Rigidbody2D rb;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("trap"))
         {
-            //rb.bodyType = RigidbodyType2D.Static;
-            //Destroy(rb.gameObject);
-            //Thread.Sleep(1000);
-            // Restarting the current level
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Die();
         }
     }
- 
+
+    public override void HandleDeathAnimDone()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }

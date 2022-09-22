c41709c72f997a43614fb9ef7f081e650ffc610c
Vacancyy <xiech@oregonstate.edu>
2022-09-22 15:31:15 -0700

    Add more buttons on level select scene

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -14,4 +14,16 @@ public class LevelSelect : MonoBehaviour
     public void LV2() {
         GameObject.FindWithTag("LevelManager").GetComponent<LevelManager>().JumpToLevel("Level2");
     }
+
+    public void LV3() {
+        GameObject.FindWithTag("LevelManager").GetComponent<LevelManager>().JumpToLevel("Level3");
+    }
+
+    public void LV4() {
+        GameObject.FindWithTag("LevelManager").GetComponent<LevelManager>().JumpToLevel("Level4");
+    }
+
+    public void LV5() {
+        GameObject.FindWithTag("LevelManager").GetComponent<LevelManager>().JumpToLevel("Level5");
+    }
 }

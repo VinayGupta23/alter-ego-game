35f6f13c303233730d0c02b6ded4b289a0f80572
venkata naresh chilakurthi <chilakur@usc.edu>
2022-09-27 10:51:38 +0530

    Renamed to MovementController

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PingPong : MonoBehaviour
+public class MovementController : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]

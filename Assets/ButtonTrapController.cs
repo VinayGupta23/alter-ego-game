25a69559c2a0f87295fb2b871e582b37140d9af0
venkata naresh chilakurthi <chilakur@usc.edu>
2022-09-28 02:58:26 -0700

    Added a prefab for creating a button controlling spikes
    A new prefab for button exclusively controlling spikes. This can be added to existing button by modifying it to take multiple targets or we can use this one

---
+++ Assets/ButtonTrapController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrapController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] targets;
+
+    IUnderControl[] controlComponents = { };
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (targets.Length > 0) {
+            foreach (var target in targets) { 
+            
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IUnderControl[] components = { };
+        foreach (var target in targets)
+        {   
+            components = target.GetComponents<IUnderControl>();
+            foreach (var component in components) {
+                component.Toggle();
+            }
+            
+        }
+    }
+}

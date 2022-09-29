0445c28ffb60fa14c8654e1c0451899c789280e2
venkata naresh chilakurthi <chilakur@usc.edu>
2022-09-28 22:10:40 -0700

    Added physics for moveable platforms

--- Assets/ButtonTrapController.cs
+++ Assets/ButtonTrapController.cs
@@ -9,15 +9,6 @@ public class ButtonTrapController : MonoBehaviour
 
     IUnderControl[] controlComponents = { };
     // Start is called before the first frame update
-    void Start()
-    {
-        if (targets.Length > 0) {
-            foreach (var target in targets) { 
-            
-            }
-        }
-        
-    }
 
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)

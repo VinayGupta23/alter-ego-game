d24fe17797476d6245d66a2f7e6e377c67ea550d
Vinay Gupta <vinayg@usc.edu>
2022-10-03 19:40:53 -0700

    Refactor all constants to a separate file

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -13,28 +13,7 @@ public class LevelManager : MonoBehaviour
 
     private static string mainMenuScene = "MainMenu";
     private static string levelSelectScene = "LevelSelect";
-    private static List<string> levels = new List<string> {
-        "0-1",
-        "0-2",
-        "0-3",
-        "1-1",
-        "1-2",
-        "1-3",
-        "2-1",
-        "2-2",
-        "2-3",
-        "2-4",
-        "2-5",
-        "3-1",
-        "3-2",
-        "3-3",
-        "4-1",
-        "4-2",
-        "4-3",
-        "5-1",
-        "5-2",
-        "5-3"
-    };
+    private static List<string> levels = Constants.LevelNames;
 
     private int current;
 

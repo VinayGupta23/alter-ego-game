de7ff1599d5a0e9f6e8f6416bed61185089e4897
EfazMuh <eem_680@usc.edu>
2022-10-04 20:39:35 -0700

    Fixed box collision platform and edge case

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -60,7 +60,9 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("MovingPlatform"))
+        if ((collision.gameObject.CompareTag("MovingPlatform") || 
+             (collision.transform.parent != null && collision.transform.parent.gameObject.CompareTag("MovingPlatform"))) && 
+            this.transform.position.y > collision.transform.position.y)
         {
             gameObject.transform.parent = collision.transform;
         }
@@ -68,7 +70,8 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("MovingPlatform"))
+        if (collision.gameObject.CompareTag("MovingPlatform") || 
+            (collision.transform.parent != null && collision.transform.parent.gameObject.CompareTag("MovingPlatform")))
         {
             gameObject.transform.parent = null;
         }

4312c193a8a3eeabb1a33a0011e21f932ba9f599
Vinay Gupta <vinayg@usc.edu>
2022-10-05 20:21:20 -0700

    Merge latest changes from 'origin/main'

--- Assets/Scripts/GemCollection.cs
+++ Assets/Scripts/GemCollection.cs
@@ -6,19 +6,30 @@ public class GemCollection: MonoBehaviour
 {
     private new SpriteRenderer renderer;
     private bool collected = false;
+
+    private bool isConfigured = false;
+    private Color fadeColor = new Color(0f, 0f, 0f, 0.7f);
+
     void Start()
     {
         renderer = GetComponent<SpriteRenderer>();
-        if (ProgressManager.Instance.GameProgress.IsGemCollected(LevelManager.Instance.GetCurrentLevel()))
+    }
+
+    private void Update()
+    {
+        // Ideally we want to do this once in Start()
+        // Since order of execution of Start() is not guaranteed,
+        // the managers may not be setup leading to race conditions.
+        if (!isConfigured)
         {
-            renderer.color = Color.grey;
-            Collider2D collider = GetComponent<Collider2D>();
-            collider.enabled = false;
-            collider.isTrigger = false;
-        }
-        else
-        {
-            renderer.color = Color.red;
+            if (ProgressManager.Instance.GameProgress.IsGemCollected(LevelManager.Instance.GetCurrentLevel()))
+            {
+                renderer.color -= fadeColor;
+                Collider2D collider = GetComponent<Collider2D>();
+                collider.enabled = false;
+                collider.isTrigger = false;
+            }
+            isConfigured = true;
         }
     }
 

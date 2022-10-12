dee326cbfc198b3834c71fde58110f3986248d8b
Ashutosh-30 <asharma7@usc.edu>
2022-10-11 18:55:14 -0700

    1. More efficient way of accessing child objects; 2. Color change for reverse spike as "red" was too difficult on eyes while moving.

--- Assets/Scripts/ReceiveStatusEffect.cs
+++ Assets/Scripts/ReceiveStatusEffect.cs
@@ -6,6 +6,8 @@ public class ReceiveStatusEffect : MonoBehaviour
 {
     private PlayerController controller;
     private SpriteRenderer render;
+    private GameObject Speed;
+    private GameObject Reverse;
     private Color statusColor = new Color(0.52f, 1f, 0.61f);
 
     public void ApplyEffect(StatusEffect statusEffect, float duration)
@@ -13,7 +15,7 @@ public class ReceiveStatusEffect : MonoBehaviour
         switch(statusEffect)
         {
             case StatusEffect.SPEED_BOOST:
-                ShowBoostSymbol("Speed");
+                ShowBoostSymbol(Speed);
                 controller.speed *= 1.5f;
                 render.color = statusColor;
                 if(duration!=0.0f){
@@ -22,7 +24,7 @@ public class ReceiveStatusEffect : MonoBehaviour
                 break;
 
             case StatusEffect.FLIP_DIRECTION:
-                ShowBoostSymbol("Reverse");
+                ShowBoostSymbol(Reverse);
                 controller.speed *= -1.0f;
                 render.color = statusColor;
                 if(duration!=0.0f){
@@ -38,8 +40,8 @@ public class ReceiveStatusEffect : MonoBehaviour
         yield return new WaitForSeconds(duration);
         controller.speed /= speedChange;
         render.color = Color.white;
-        HideBoostSymbol("Speed");
-        HideBoostSymbol("Reverse");
+        HideBoostSymbol(Speed);
+        HideBoostSymbol(Reverse);
     }
 
     // Start is called before the first frame update
@@ -47,17 +49,19 @@ public class ReceiveStatusEffect : MonoBehaviour
     {
         controller = GetComponent<PlayerController>();
         render = GetComponent<SpriteRenderer>();
-        HideBoostSymbol("Speed");
-        HideBoostSymbol("Reverse");
+        Speed = this.gameObject.transform.Find("Speed").gameObject;
+        Reverse = this.gameObject.transform.Find("Reverse").gameObject;
+        HideBoostSymbol(Speed);
+        HideBoostSymbol(Reverse);
     }
 
-    void ShowBoostSymbol(string boostName)
+    void ShowBoostSymbol(GameObject boostGameObject)
     {
-        this.gameObject.transform.Find(boostName).gameObject.SetActive(true);
+        boostGameObject.SetActive(true);
     }
     
-    void HideBoostSymbol(string boostName)
+    void HideBoostSymbol(GameObject boostGameObject)
     {
-        this.gameObject.transform.Find(boostName).gameObject.SetActive(false);
+        boostGameObject.SetActive(false);
     }
 }

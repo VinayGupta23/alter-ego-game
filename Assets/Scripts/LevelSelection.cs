a86c24b9b365622928d335cad9d0be0e2312ec72
rustin <rustinzhao@gmail.com>
2022-10-17 22:20:55 -0700

    tick script

--- Assets/Scripts/LevelSelection.cs
+++ Assets/Scripts/LevelSelection.cs
@@ -7,7 +7,8 @@ using TMPro;
 public class LevelSelection : MonoBehaviour
 {
     private string level;
-    
+    Toggle m_Toggle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +18,14 @@ public class LevelSelection : MonoBehaviour
 
     public void OpenScene(){
         LevelManager.Instance.JumpToLevel(level);
+
+        m_Toggle = GetComponent<Toggle>();
+
+        if (ProgressManager.Instance.GameProgress.IsCompleted(level)){
+            m_Toggle.isOn = true;
+        }
         
     }
-
-    
 
 }
 

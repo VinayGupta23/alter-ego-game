f797fa5f338c943c782f6607d5cc82b8e340e3a0
EfazMuh <eem_680@usc.edu>
2022-10-18 22:49:59 -0700

    Added pill duration indicators

--- Assets/Scripts/ApplyStatusEffect.cs
+++ Assets/Scripts/ApplyStatusEffect.cs
@@ -13,6 +13,18 @@ public class ApplyStatusEffect : MonoBehaviour
     public StatusEffect statusEffect;
     public float statusDuration;
 
+    void Start()
+    {
+        if (statusDuration != 0.0f)
+        {
+            this.gameObject.transform.Find("Infinite").gameObject.SetActive(false);
+        }
+        else
+        {
+            this.gameObject.transform.Find("Timed").gameObject.SetActive(false);
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other){
          ReceiveStatusEffect receiver = other.gameObject.GetComponent<ReceiveStatusEffect>();
          if(receiver!=null)

0c524223babc5f63bfc272a6c68a04b1a146c4ad
Vinay Gupta <vinayg@usc.edu>
2022-10-19 00:00:18 -0700

    Improve death animation to show ripple effect

--- Assets/Scripts/LifeBase.cs
+++ Assets/Scripts/LifeBase.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class LifeBase : MonoBehaviour
 {
     protected Animator animator;
+    [SerializeField]
+    protected GameObject deathRingPrefab;
+    [SerializeField]
+    protected Color deathRingTint;
     protected Rigidbody2D rb;
     protected RigidbodyType2D originalBodyType;
 
@@ -30,9 +34,17 @@ public class LifeBase : MonoBehaviour
             // Player is already dead, do nothing
             return;
         }
+
         _isAlive = false;
         rb.bodyType = RigidbodyType2D.Static;
+
         animator.SetTrigger("Die");
+        if (deathRingPrefab != null)
+        {
+            GameObject deathRing = GameObject.Instantiate(deathRingPrefab);
+            deathRing.transform.position = transform.position;
+            deathRing.GetComponent<SpriteRenderer>().color = deathRingTint;
+        }
     }
 
     public void Revive()

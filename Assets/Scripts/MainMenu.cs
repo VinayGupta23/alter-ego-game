ff949c555c05ab6389ef9fd407a546bd7e1c9e97
Cheng Xie <chengxie@Chengs-Air.lan>
2022-11-02 17:38:00 -0700

    Add sound to main menu buttons

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
     public void LevelSelect() {
         LevelManager.Instance.LevelSelect();
+        SFXManager.SFXInstance.Audio.PlayOneShot(SFXManager.SFXInstance.Click);
+
     }
 
     public void StartTutorial() {
         LevelManager.Instance.JumpToLevel("0-1");
+        SFXManager.SFXInstance.Audio.PlayOneShot(SFXManager.SFXInstance.Click);
+
     }
 }
